ee0608d9ed48c3b1f959143438242e89e7b1620d
Owen McCann <owenmccann2014@gmail.com>
2024-07-08 19:29:03 +0100

    first commit

---
+++ HelloWorld.py
@@ -0,0 +1,11 @@
+name =input('Enter name: ') #input your name
+print('hello!') #can use single or double quotes
+print(name) #print variable
+x = 9 + 8
+x += x #same as java
+x = x+1 #same as x++ in java as no x++ in python
+print(x)
+y = x/0 #error divide by 0
+print('it's a nice day today') 
+# won't work because the string has a single quote in it
+print("it's a nice day today") # works fine now due to the quote type being differnt
